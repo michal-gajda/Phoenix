3bb6d9601fc267f129216654513a75d5655d6e2f
Michal Gajda <michal@gajda.co.uk>
2024-09-27 00:36:38 +0100

    Add OpenTelemetry

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,10 +1,26 @@
 namespace Phoenix.WebApi;
 
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 public static class Program
 {
+    private const string SERVICE_NAME = "Phoenix";
+
     public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(SERVICE_NAME)).AddOtlpExporter();
+        });
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(SERVICE_NAME))
+            .WithTracing(tracing => tracing.AddAspNetCoreInstrumentation().AddOtlpExporter())
+            .WithMetrics(metrics => metrics.AddAspNetCoreInstrumentation().AddOtlpExporter());
 
         builder.Services.AddHealthChecks();
 
